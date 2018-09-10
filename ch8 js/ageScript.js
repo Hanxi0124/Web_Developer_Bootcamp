34c4923494cde6a724202f253b0d2cf3a64cac5b
Hanxi <30990626+Hanxi0124@users.noreply.github.com>
2018-09-10 00:07:51 -0400

    Chapter 8

---
+++ ch8 js/ageScript.js
@@ -0,0 +1,3 @@
+var age = prompt("What's your age?")
+var days = 365.25 * age;  // every 4 years there will be a 366 day
+alert("You have lived for " + days + " days.")
---
+++ ch8 js/gameScript.js
@@ -0,0 +1,13 @@
+var secretnumber = 4;
+
+var guess = prompt("Guess?");
+
+if (Number(guess) === secretnumber){
+
+	alert("Right!");
+
+}
+
+else {
+	alert("WRONG!");
+}
