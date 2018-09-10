11e24f12ac1f018334e386c8fc41c395cabb09fc
Hanxi <30990626+Hanxi0124@users.noreply.github.com>
2018-09-10 00:09:02 -0400

    Chapter 9

---
+++ ch9/AnnoyScript.js
@@ -0,0 +1,7 @@
+var answer = prompt("Yes or No");
+
+while(answer.indexOf("Yes") < 0){
+ 	var answer = prompt("Yes or No");
+}
+
+alert("Yes");
---
+++ ch9/loopscript.js
@@ -0,0 +1,69 @@
+console.log("Question1");
+var i = -10;
+
+while(i < 20){
+	console.log(i);
+	i++;
+}
+
+console.log("Question2");
+var i = 10;
+
+while(i <= 40){
+	if (i % 2 == 0){
+		console.log(i);
+	}
+	i++;
+}
+
+console.log("Question3");
+var i = 300;
+
+while(i <= 333){
+	if (!(i % 2 == 0)){
+		console.log(i);
+	}
+	i++;
+}
+
+console.log("Question4");
+var i = 5;
+
+while(i <= 50){
+	if ((i % 5 == 0) && (i % 3 == 0)){
+		console.log(i);
+	}
+	i++;
+}
+
+
+console.log("Question1");
+for (var i = -10; i < 20; i++) {
+	console.log(i);
+}
+
+console.log("Question2");
+for (var i = 10; i < 41; i++) {
+	if (i % 2 == 0) {
+		console.log(i);
+	}
+
+}
+
+console.log("Question3");
+
+for (var i = 300; i < 334; i++) {
+	if (i % 2 !== 0) {
+		console.log(i);
+	}
+
+}
+
+console.log("Question4");
+
+for (var i = 5; i < 51; i++) {
+	if ((i % 5 == 0) && (i % 3 == 0)){
+		console.log(i);
+	}
+
+}
