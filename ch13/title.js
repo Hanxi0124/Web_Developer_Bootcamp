3428cd80db66b15f3289f04182fe9222dba633ce
Hanxi <30990626+Hanxi0124@users.noreply.github.com>
2018-09-10 00:10:31 -0400

    Chapter 13

---
+++ ch13/title.js
@@ -0,0 +1,27 @@
+var posts = {
+	{
+		title: "Cats",
+		author: "Hanxi",
+		comments:["Awesome","Bad"]
+	},
+	{
+		title:"Dhoogs",
+		authot:"Qin",
+		comments:["Wonderful","Lovely"]
+	}
+} 
+
+for(int i = 0; i<movies.length; i++){
+	if(movies[i].watch){
+		console.log("You have wathced " + movies[i].title + " - " + movies[i].rating + " stars.");
+	}
+	else {
+		console.log("You have not watched " + movies[i].title + " - " + movies[i].rating + " stars.");
+	}
+}
+
+
+var comments = {};
+comments.data = ["Good Job!","Bye"]
+
+
